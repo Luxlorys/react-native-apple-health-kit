a4f6f0f43ebcf1ec146f03f8c14e2b04e3db0f23
Andrei <andrei@MacBook-Pro-Andrei.local>
2025-02-23 21:30:53 +0200

    feat: methods placeholders for android

--- android/src/main/java/com/applehealthkit/AppleHealthKitModule.kt
+++ android/src/main/java/com/applehealthkit/AppleHealthKitModule.kt
@@ -1,5 +1,6 @@
 package com.applehealthkit
 
+import com.facebook.react.bridge.Promise
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
 
@@ -11,10 +12,29 @@ class AppleHealthKitModule(reactContext: ReactApplicationContext) :
     return NAME
   }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  override fun multiply(a: Double, b: Double): Double {
-    return a * b
+  // Placeholder for requestHealthKitPermissions
+  override fun requestHealthKitPermissions(promise: Promise) {
+    promise.reject("Unsupported", "HealthKit is not supported on Android.")
+  }
+
+  // Placeholder for getSteps
+  override fun getSteps(daysBefore: Double, promise: Promise) {
+    promise.reject("Unsupported", "HealthKit is not supported on Android.")
+  }
+
+  // Placeholder for getHeartRate
+  override fun getHeartRate(daysBefore: Double, promise: Promise) {
+    promise.reject("Unsupported", "HealthKit is not supported on Android.")
+  }
+
+  // Placeholder for getMeasurement
+  override fun getMeasurement(promise: Promise) {
+    promise.reject("Unsupported", "HealthKit is not supported on Android.")
+  }
+
+  // Placeholder for getAppleMoveTime
+  override fun getAppleMoveTime(daysBefore: Double, promise: Promise) {
+    promise.reject("Unsupported", "HealthKit is not supported on Android.")
   }
 
   companion object {
